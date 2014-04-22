79ec1b751f6b74941d418544ea4cd5699059bfa4
Compx <mac@mactekiMacBook-Pro.local>
2014-04-22 15:39:08 +0800

    Initial Commit

---
+++ bleOneToMore/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  bleOneToMore
+//
+//  Created by Compx on 14-4-22.
+//  Copyright (c) 2014年 compx.com.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
